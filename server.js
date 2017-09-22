f862c6d4d729b30ecfc6c9c0ea5679cce1168083
yotoo2920 <d.orta.santana@ufl.edu>
2017-09-21 23:01:32 -0400

    port setup by heroku

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 var app = require('./server/config/app');
 var server = app.start();
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
